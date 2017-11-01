a1fbb21ab1439b811d3d8f7122e94bb72eeed3d8
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-11-01 17:35:38 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,12 +1,15 @@
 class Backer
-  attr_accessor :projects, :name
+  attr_accessor :projects, :name, :backed_projects
 
   def initialize(name)
     @name = name
     @backed_projects = []
   end
 
-  def back_project(new_project)
+  def back_project(title)
+    new_project = Project.new(title)
+    @backed_projects << new_project
+    
 
   end
 
